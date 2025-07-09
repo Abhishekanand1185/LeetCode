18d928edd117c3633d777dac9b69e68376d7f1b0
Abhishek Anand <146563172+Abhishekanand1185@users.noreply.github.com>
2025-07-09 16:00:31 +0530

    Time: 15 ms (61.58%), Space: 44.6 MB (92.82%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.java
+++ 0741-cherry-pickup/0741-cherry-pickup.java
@@ -1,53 +1,56 @@
 class Solution {
     public int cherryPickup(int[][] grid) {
-        int row1 = grid.length;
-        int col1 = grid[0].length;
-        int row2 = grid.length;
-        int col2 = grid[0].length;
-
-         int dp[][][][] = new int[row1][col1][row2][col2];
-         int res = cherryPickupHelper(grid, 0, 0, 0, 0, dp);
-         if(res<0){
+       int dp[][][] = new int[grid.length][grid[0].length][grid.length];
+        for (int i = 0; i < dp.length; i++) {
+        for (int j = 0; j < dp[0].length; j++) {
+            Arrays.fill(dp[i][j], -1);
+            }
+        }
+         int res = cherryPickupHelper(grid, 0, 0, 0, dp);
+         if(res == Integer.MIN_VALUE){
             return 0;
-         }
-         return res;
-         
+        }else{
+            return res;
+        }
     }
-    public static int cherryPickupHelper(int grid[][], int row1, int col1, int row2, int col2, int dp[][][][]){
-
+    public static int cherryPickupHelper(int grid[][], int row1, int col1, int row2, int dp[][][]){
+          int col2 = row1 + col1 - row2;
           if(row1 >= grid.length || row2 >= grid.length || 
-           col1 >= grid.length || col2 >= grid.length){
-            return Integer.MIN_VALUE;
-          }
-        
-          if(grid[row1][col1] == -1 || grid[row2][col2] == -1){
+           col1 >= grid[0].length || col2 >= grid[0].length
+           || grid[row1][col1] == -1 || grid[row2][col2] == -1){
             return Integer.MIN_VALUE;
           }
 
           if(row1 == grid.length-1 && col1 == grid[0].length-1 
-          && row1 == grid.length-1 && col1 == grid[0].length-1){
+          && row2 == grid.length-1 && col2 == grid[0].length-1){
             return grid[row1][col1];
           }
 
-          if(dp[row1][col1][row2][col2] != 0){
-            return dp[row1][col1][row2][col2];
+          if(dp[row1][col1][row2] != -1){
+            return dp[row1][col1][row2];
           }
-          int path1 = cherryPickupHelper(grid, row1, col1+1, row2, col2+1, dp);
-          int path2 = cherryPickupHelper(grid, row1, col1+1, row2+1, col2, dp);
-          int path3 = cherryPickupHelper(grid, row1+1, col1, row2, col2+1, dp);
-          int path4 = cherryPickupHelper(grid, row1+1, col1, row2+1, col2, dp);
 
-          int ans = Math.max(Math.max(path1, path2),Math.max(path3, path4));
+
+          int path1 = cherryPickupHelper(grid, row1, col1+1, row2, dp);
+          int path2 = cherryPickupHelper(grid, row1, col1+1, row2+1, dp);
+          int path3 = cherryPickupHelper(grid, row1+1, col1, row2, dp);
+          int path4 = cherryPickupHelper(grid, row1+1, col1, row2+1, dp);
+
+          int max = Math.max(Math.max(path1, path2),Math.max(path3, path4));
           int contri = 0;
-
           if(row1 == row2 && col1 == col2){
             contri = grid[row1][col1];
           }
           else{
             contri = grid[row1][col1] + grid[row2][col2];
           }
+          if(max == Integer.MIN_VALUE){
+            dp[row1][col1][row2] = Integer.MIN_VALUE;
+            return Integer.MIN_VALUE;
 
-          dp[row1][col1][row2][col2] = ans + contri;
-          return ans + contri;
+          }else{
+             dp[row1][col1][row2] = max+contri;
+             return max+contri;
+          }
     }
 }
