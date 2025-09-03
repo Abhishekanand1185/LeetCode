42326470ede7b0b697633488ae8ea9299f754fb0
Abhishek Anand <146563172+Abhishekanand1185@users.noreply.github.com>
2025-09-04 01:57:01 +0530

    Time: 23 ms (80.2%), Space: 55.6 MB (30.12%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -1,29 +1,21 @@
 class Solution {
     public int findMaxLength(int[] nums) {
        int n = nums.length;
-       int pcz[] = new int[n];
-       int pco[] = new int[n];
-       int zero = 0;
-       int one = 0;
-       for(int i=0; i<nums.length; i++){
-            if(nums[i] == 1){
-                one++;
-            }
-            else if(nums[i] == 0){
-                zero++;
-            }
-           pcz[i] = zero;
-           pco[i] = one;
-       }
        HashMap<Integer, Integer> hm = new HashMap<>();
        hm.put(0,-1);
        int ans = 0;
+       int count = 0;
        for(int e=0; e<n; e++){
-            int diff = pco[e] - pcz[e];
-            if(hm.containsKey(diff)){
-                 ans = Math.max(ans, e - hm.get(diff));
+            if(nums[e] == 1){
+                count++;
+            }else{
+                count--;
             }
-            hm.put(diff,e);
+            if(hm.containsKey(count)){
+                ans = Math.max(ans, e - hm.get(count));
+            }else{
+                hm.put(count,e);
+            }
        }
         
     return ans;
