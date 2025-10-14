25833c9e7b1e1bc9f4eba45ae0bfee200c926b71
Abhishek Anand <146563172+Abhishekanand1185@users.noreply.github.com>
2025-10-14 13:36:21 +0530

    Time: 3 ms (95.74%), Space: 44.1 MB (79.36%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int ans = 0;
+        int n = nums.length;
+        int dp[] = new int[n];
+        for(int i=0; i<n; i++){
+            int low = 0;
+            int high = ans;
+            while(low < high){
+                int mid = low+(high-low)/2;
+                if(dp[mid] < nums[i]){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid;
+                }
+            }
+            dp[low] = nums[i];
+            if(low == ans){
+                ans++;
+            }
+        }
+    return ans;
+    }
+}
