d44779059aa93858867d53a6204ffbfe70d451e9
Abhishek Anand <146563172+Abhishekanand1185@users.noreply.github.com>
2025-12-09 23:53:09 +0530

    Time: 188 ms (73.64%), Space: 186.2 MB (47.29%) - LeetHub

---
+++ 3583-count-special-triplets/3583-count-special-triplets.java
@@ -0,0 +1,20 @@
+class Solution {
+    static final int M = (int)1e9 + 7;
+    public int specialTriplets(int[] nums) {
+        HashMap<Integer, Integer> validI = new HashMap<>();
+        HashMap<Integer, Integer> validJ = new HashMap<>();
+        int res = 0;
+        for(int num : nums){
+            if(num % 2 == 0){
+                res = (res + validJ.getOrDefault(num/2, 0)) % M;
+            }
+
+            int updatedJ = (validJ.getOrDefault(num, 0) + validI.getOrDefault(num*2, 0)) % M;
+
+            validJ.put(num, updatedJ);
+
+            validI.put(num, validI.getOrDefault(num, 0) + 1);
+        }
+    return res;
+    }
+}
