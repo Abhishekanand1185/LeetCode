5c2662ae3dd5a758206a4a9ab03e70147aeb9896
Abhishek Anand <146563172+Abhishekanand1185@users.noreply.github.com>
2025-12-11 22:04:15 +0530

    Time: 1132 ms (5%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,14 +1,9 @@
 # Write your MySQL query statement below
-# here in this line we are selecting all 4 column we need but with table name
-# so here i define p for person table in line number 4 so so insted of person.lastName
-# I can write p.lastName and same for addredd table
-select p.lastName, p.firstName, a.city, a.state 
-# Here we define person table as p and use from(in that table we will do left join)
-from person p 
-# Left join we use to Join two table's so here we use left join means we are join 2nd table in 
-# 1st table
-left join 
-# Here we define address table as a and also we use this table to join in first table
-address a 
-# Here we are telling that personId is same in both table so you can match that and give me output
+SELECT 
+    p.firstName,
+    p.lastName,
+    a.city,
+    a.state
+FROM Person p 
+LEFT JOIN Address a 
 on p.personId = a.personId;
